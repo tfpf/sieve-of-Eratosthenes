4d42002a61025d3758de9d81e146ecd27600fd3c
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-13 00:37:43 +0530

    Sieve of Atkin.

---
+++ sieve_of_atkin/sieve_of_atkin.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/******************************************************************************
+ * Main function.
+ *****************************************************************************/
+int main(int const argc, char const *argv[])
+{
+    return EXIT_SUCCESS;
+}
