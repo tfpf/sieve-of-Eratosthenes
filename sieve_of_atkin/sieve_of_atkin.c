864997b0e14228bb9dc0b4f9fcdbb97949349603
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-13 19:32:11 +0530

    Turned algorithms 3.1, 3.2, 3.3, 4.1, 4.2 and 4.3 into lookups.

--- sieve_of_atkin/sieve_of_atkin.c
+++ sieve_of_atkin/sieve_of_atkin.c
@@ -21,6 +21,70 @@ static short unsigned shifts[] = {16, 0, 16, 16, 16, 16, 16, 1, 16, 16, 16, 2, 1
 // no algorithms should be applied.)
 static short algorithm[] = {0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 3, 0, 1, 0, 0, 0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 2, 0, 0, 0, 3, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 3};
 
+// Indices are residues; values are arrays with which to begin the search.
+static short triplets[][24][3] = {
+    {},
+    {{2, 15, 4}, {3, 5, 1}, {3, 25, 11}, {5, 9, 3}, {5, 21, 9}, {7, 15, 7}, {8, 15, 8}, {10, 9, 8}, {10, 21, 14}, {12, 5, 10}, {12, 25, 20}, {13, 15, 15}, {15, 1, 15}, {15, 11, 17}, {15, 19, 21}, {15, 29, 29}},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {{1, 2, 0}, {1, 8, 1}, {1, 22, 8}, {1, 28, 13}, {3, 10, 2}, {3, 20, 7}, {7, 10, 4}, {7, 20, 9}, {9, 2, 4}, {9, 8, 5}, {9, 22, 12}, {9, 28, 17}},
+    {},
+    {},
+    {},
+    {{2, 1, 0}, {2, 11, -2}, {2, 19, -6}, {2, 29, -14}, {3, 4, 0}, {3, 14, -3}, {3, 16, -4}, {3, 26, -11}, {5, 2, 1}, {5, 8, 0}, {5, 22, -7}, {5, 28, -12}, {7, 4, 2}, {7, 14, -1}, {7, 16, -2}, {7, 26, -9}, {8, 1, 3}, {8, 11, 1}, {8, 19, -3}, {8, 29, -11}, {10, 7, 4}, {10, 13, 2}, {10, 17, 0}, {10, 23, -4}},
+    {},
+    {{1, 3, 0}, {1, 27, 12}, {4, 3, 1}, {4, 27, 13}, {6, 7, 3}, {6, 13, 5}, {6, 17, 7}, {6, 23, 11}, {9, 7, 6}, {9, 13, 8}, {9, 17, 10}, {9, 23, 14}, {11, 3, 8}, {11, 27, 20}, {14, 3, 13}, {14, 27, 25}},
+    {},
+    {},
+    {},
+    {{2, 1, 0}, {2, 11, 2}, {2, 19, 6}, {2, 29, 14}, {7, 1, 3}, {7, 11, 5}, {7, 19, 9}, {7, 29, 17}, {8, 1, 4}, {8, 11, 6}, {8, 19, 10}, {8, 29, 18}, {13, 1, 11}, {13, 11, 13}, {13, 19, 17}, {13, 29, 25}},
+    {},
+    {{1, 4, 0}, {1, 14, 3}, {1, 16, 4}, {1, 26, 11}, {5, 2, 1}, {5, 8, 2}, {5, 22, 9}, {5, 28, 14}, {9, 4, 4}, {9, 14, 7}, {9, 16, 8}, {9, 26, 15}},
+    {},
+    {},
+    {},
+    {{1, 10, -2}, {1, 20, -7}, {2, 7, -1}, {2, 13, -3}, {2, 17, -5}, {2, 23, -9}, {3, 2, 0}, {3, 8, -1}, {3, 22, -8}, {3, 28, -13}, {4, 5, 0}, {4, 25, -10}, {6, 5, 1}, {6, 25, -9}, {7, 2, 2}, {7, 8, 1}, {7, 22, -6}, {7, 28, -11}, {8, 7, 2}, {8, 13, 0}, {8, 17, -2}, {8, 23, -6}, {9, 10, 2}, {9, 20, -3}},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {{1, 5, 0}, {1, 25, 10}, {4, 5, 1}, {4, 25, 11}, {5, 7, 2}, {5, 13, 4}, {5, 17, 6}, {5, 23, 10}, {10, 7, 7}, {10, 13, 9}, {10, 17, 11}, {10, 23, 15}, {11, 5, 8}, {11, 25, 18}, {14, 5, 13}, {14, 25, 23}},
+    {},
+    {{3, 2, 0}, {3, 8, 1}, {3, 22, 8}, {3, 28, 13}, {5, 4, 1}, {5, 14, 4}, {5, 16, 5}, {5, 26, 12}, {7, 2, 2}, {7, 8, 3}, {7, 22, 10}, {7, 28, 15}},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {{2, 9, 1}, {2, 21, 7}, {3, 1, 0}, {3, 11, 2}, {3, 19, 6}, {3, 29, 14}, {7, 9, 4}, {7, 21, 10}, {8, 9, 5}, {8, 21, 11}, {12, 1, 9}, {12, 11, 11}, {12, 19, 15}, {12, 29, 23}, {13, 9, 12}, {13, 21, 18}},
+    {},
+    {},
+    {},
+    {{2, 5, 0}, {2, 25, 10}, {5, 1, 1}, {5, 11, 3}, {5, 19, 7}, {5, 29, 15}, {7, 5, 3}, {7, 25, 13}, {8, 5, 4}, {8, 25, 14}, {10, 1, 6}, {10, 11, 8}, {10, 19, 12}, {10, 29, 20}, {13, 5, 11}, {13, 25, 21}},
+    {},
+    {{1, 10, 1}, {1, 20, 6}, {3, 4, 0}, {3, 14, 3}, {3, 16, 4}, {3, 26, 11}, {7, 4, 2}, {7, 14, 5}, {7, 16, 6}, {7, 26, 13}, {9, 10, 5}, {9, 20, 10}},
+    {},
+    {},
+    {},
+    {{1, 4, -1}, {1, 14, -4}, {1, 16, -5}, {1, 26, -12}, {2, 5, -1}, {2, 25, -11}, {3, 10, -2}, {3, 20, -7}, {4, 1, 0}, {4, 11, -2}, {4, 19, -6}, {4, 29, -14}, {6, 1, 1}, {6, 11, -1}, {6, 19, -5}, {6, 29, -13}, {7, 10, 0}, {7, 20, -5}, {8, 5, 2}, {8, 25, -8}, {9, 4, 3}, {9, 14, 0}, {9, 16, -1}, {9, 26, -8}},
+    {},
+    {{1, 15, 3}, {4, 15, 4}, {5, 3, 1}, {5, 27, 13}, {6, 5, 2}, {6, 25, 12}, {9, 5, 5}, {9, 25, 15}, {10, 3, 6}, {10, 27, 18}, {11, 15, 11}, {14, 15, 16}, {15, 7, 15}, {15, 13, 17}, {15, 17, 19}, {15, 23, 23}},
+    {},
+    {},
+    {},
+    {{1, 7, 0}, {1, 13, 2}, {1, 17, 4}, {1, 23, 8}, {4, 7, 1}, {4, 13, 3}, {4, 17, 5}, {4, 23, 9}, {11, 7, 8}, {11, 13, 10}, {11, 17, 12}, {11, 23, 16}, {14, 7, 13}, {14, 13, 15}, {14, 17, 17}, {14, 23, 21}},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {{1, 2, -1}, {1, 8, -2}, {1, 22, -9}, {1, 28, -14}, {4, 7, -1}, {4, 13, -3}, {4, 17, -5}, {4, 23, -9}, {5, 4, 0}, {5, 14, -3}, {5, 16, -4}, {5, 26, -11}, {6, 7, 0}, {6, 13, -2}, {6, 17, -4}, {6, 23, -8}, {9, 2, 3}, {9, 8, 2}, {9, 22, -5}, {9, 28, -10}, {10, 1, 4}, {10, 11, 2}, {10, 19, -2}, {10, 29, -10}},
+};
+
 /******************************************************************************
  * Algorithm 4.1.
  *
@@ -29,10 +93,10 @@ static short algorithm[] = {0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 3, 0, 1, 0, 0, 0, 1
  * @param delta What each of the prime numbers must be congruent to modulo 60.
  * @param x Starting abcissa.
  * @param y0 Starting ordinate.
+ * @param k0 Starting index into `sieve`.
  *****************************************************************************/
-void sieve_of_atkin_algorithm_4_1(uint16_t *sieve, size_t sieve_len, int delta, long long x, long long y0)
+void sieve_of_atkin_algorithm_4_1(uint16_t *sieve, size_t sieve_len, int delta, long long x, long long y0, long long k0)
 {
-    long long k0 = (4 * x * x + y0 * y0 - delta) / 60;
     while((long long unsigned)k0 < sieve_len)
     {
         k0 += 2 * x + 15;
@@ -68,10 +132,10 @@ void sieve_of_atkin_algorithm_4_1(uint16_t *sieve, size_t sieve_len, int delta, 
  * @param delta What each of the prime numbers must be congruent to modulo 60.
  * @param x Starting abcissa.
  * @param y0 Starting ordinate.
+ * @param k0 Starting index into `sieve`.
  *****************************************************************************/
-void sieve_of_atkin_algorithm_4_2(uint16_t *sieve, size_t sieve_len, int delta, long long x, long long y0)
+void sieve_of_atkin_algorithm_4_2(uint16_t *sieve, size_t sieve_len, int delta, long long x, long long y0, long long k0)
 {
-    long long k0 = (3 * x * x + y0 * y0 - delta) / 60;
     while((long long unsigned)k0 < sieve_len)
     {
         k0 += x + 5;
@@ -107,10 +171,10 @@ void sieve_of_atkin_algorithm_4_2(uint16_t *sieve, size_t sieve_len, int delta, 
  * @param delta What each of the prime numbers must be congruent to modulo 60.
  * @param x Starting abcissa.
  * @param y0 Starting ordinate.
+ * @param k0 Starting index into `sieve`.
  *****************************************************************************/
-void sieve_of_atkin_algorithm_4_3(uint16_t *sieve, size_t sieve_len, int delta, long long x, long long y0)
+void sieve_of_atkin_algorithm_4_3(uint16_t *sieve, size_t sieve_len, int delta, long long x, long long y0, long long k0)
 {
-    long long k0 = (3 * x * x - y0 * y0 - delta) / 60;
     while(true)
     {
         // It can be negative here, so check its sign before casting it to an
@@ -145,15 +209,9 @@ void sieve_of_atkin_algorithm_4_3(uint16_t *sieve, size_t sieve_len, int delta, 
  *****************************************************************************/
 void sieve_of_atkin_algorithm_3_1(uint16_t *sieve, size_t sieve_len, int delta)
 {
-    for(int f = 1; f <= 15; ++f)
+    for(size_t i = 0; i < 16; ++i)
     {
-        for(int g = 1; g <= 30; ++g)
-        {
-            if(delta == (4 * f * f + g * g) % 60)
-            {
-                sieve_of_atkin_algorithm_4_1(sieve, sieve_len, delta, f, g);
-            }
-        }
+        sieve_of_atkin_algorithm_4_1(sieve, sieve_len, delta, triplets[delta][i][0], triplets[delta][i][1], triplets[delta][i][2]);
     }
 }
 
@@ -166,15 +224,9 @@ void sieve_of_atkin_algorithm_3_1(uint16_t *sieve, size_t sieve_len, int delta)
  *****************************************************************************/
 void sieve_of_atkin_algorithm_3_2(uint16_t *sieve, size_t sieve_len, int delta)
 {
-    for(int f = 1; f <= 10; ++f)
+    for(size_t i = 0; i < 12; ++i)
     {
-        for(int g = 1; g <= 30; ++g)
-        {
-            if(delta == (3 * f * f + g * g) % 60)
-            {
-                sieve_of_atkin_algorithm_4_2(sieve, sieve_len, delta, f, g);
-            }
-        }
+        sieve_of_atkin_algorithm_4_2(sieve, sieve_len, delta, triplets[delta][i][0], triplets[delta][i][1], triplets[delta][i][2]);
     }
 }
 
@@ -187,18 +239,9 @@ void sieve_of_atkin_algorithm_3_2(uint16_t *sieve, size_t sieve_len, int delta)
  *****************************************************************************/
 void sieve_of_atkin_algorithm_3_3(uint16_t *sieve, size_t sieve_len, int delta)
 {
-    for(int f = 1; f <= 10; ++f)
+    for(size_t i = 0; i < 24; ++i)
     {
-        for(int g = 1; g <= 30; ++g)
-        {
-            // We need residues moduo 60, so make negative remainders positive.
-            int remainder = (3 * f * f - g * g) % 60;
-            remainder = remainder < 0 ? remainder + 60 : remainder;
-            if(delta == remainder)
-            {
-                sieve_of_atkin_algorithm_4_3(sieve, sieve_len, delta, f, g);
-            }
-        }
+        sieve_of_atkin_algorithm_4_3(sieve, sieve_len, delta, triplets[delta][i][0], triplets[delta][i][1], triplets[delta][i][2]);
     }
 }
 
