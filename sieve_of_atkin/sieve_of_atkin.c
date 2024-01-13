5193e724332902cde0abb07698f6d1289a6002a1
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-13 10:09:03 +0530

    Somewhat working for 3.1.

--- sieve_of_atkin/sieve_of_atkin.c
+++ sieve_of_atkin/sieve_of_atkin.c
@@ -1,28 +1,139 @@
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+// A number may be prime if it has one of these residues modulo 60. Otherwise,
+// it is definitely composite. (Exceptions are 2, 3 and 5, which are handled
+// separately.)
+static int residues[] = {1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59};
+
+// In this lookup table, indices are the above residues, and values are their
+// positions in the above array. Values at other indices are -1.
+static int shifts[] = {-1, 0, -1, -1, -1, -1, -1, 1, -1, -1, -1, 2, -1, 3, -1, -1, -1, 4, -1, 5, -1, -1, -1, 6, -1, -1, -1, -1, -1, 7, -1, 8, -1, -1, -1, -1, -1, 9, -1, -1, -1, 10, -1, 11, -1, -1, -1, 12, -1, 13, -1, -1, -1, 14, -1, -1, -1, -1, -1, 15};
+
+/******************************************************************************
+ * Algorithm 4.1.
+ *
+ * @param sieve Sieve of Atkin.
+ * @param sieve_len Its length.
+ * @param delta What each of the prime numbers must be congruent to modulo 60.
+ * @param x Starting abcissa.
+ * @param y0 Starting ordinate.
+ *****************************************************************************/
+void sieve_of_atkin_algorithm_4_1(uint16_t *sieve, size_t sieve_len, int delta, int x, int y0)
+{
+    int k0 = (4 * x * x + y0 * y0 - delta) / 60;
+    while((size_t)k0 < sieve_len)
+    {
+        // TODO Prevent overflow by rejecting large sieve lengths.
+        k0 += 2 * x + 15;
+        x += 15;
+    }
+    while(true)
+    {
+        x -= 15;
+        k0 -= 2 * x + 15;
+        if(x <= 0)
+        {
+            return;
+        }
+        while(k0 < 0)
+        {
+            k0 += y0 + 15;
+            y0 += 30;
+        }
+        for(size_t k = k0, y = y0; k < sieve_len; k += y + 15, y += 30)
+        {
+            sieve[k] ^= 1 << shifts[delta];
+        }
+    }
+}
+
+/******************************************************************************
+ * Algorithm 3.1.
+ *
+ * @param sieve Sieve of Atkin.
+ * @param sieve_len Its length.
+ * @param delta What each of the prime numbers must be congruent to modulo 60.
+ *****************************************************************************/
+void sieve_of_atkin_algorithm_3_1(uint16_t *sieve, size_t sieve_len, int delta)
+{
+    for(int f = 1; f <= 15; ++f)
+    {
+        for(int g = 1; g <= 30; ++g)
+        {
+            if(delta == (4 * f * f + g * g) % 60)
+            {
+                sieve_of_atkin_algorithm_4_1(sieve, sieve_len, delta, f, g);
+            }
+        }
+    }
+}
+
+/******************************************************************************
+ * Generate the sieve of Atkin.
+ *
+ * @param limit Non-strict upper bound up to which primes will be found.
+ *
+ * @return Sieve of Atkin.
+ *****************************************************************************/
+uint16_t *sieve_of_atkin(size_t limit)
+{
+    // Strict upper bound till which primes will be calculated.
+    size_t limit_rounded = limit - limit % 60 + 60;
+    if(limit_rounded <= limit)
+    {
+        fprintf(stderr, "Overflow detected. Argument is too large.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t sieve_len = limit / 60 + 1;
+    uint16_t *sieve = calloc(sieve_len, sizeof *sieve);
+    for(int delta = 0; delta < 60; ++delta)
+    {
+        switch(delta)
+        {
+            case 1: case 13: case 17: case 29: case 37: case 41: case 49: case 53:
+            sieve_of_atkin_algorithm_3_1(sieve, sieve_len, delta);
+        }
+    }
+
+    return sieve;
+}
 
 /******************************************************************************
  * Main function.
  *****************************************************************************/
 int main(int const argc, char const *argv[])
 {
-    // Non-strict upper bound till which primes will be calculated.
     size_t limit = 100;
     if(argc > 1)
     {
         char *endptr;
         long long unsigned limit_ = strtoull(argv[1], &endptr, 10);
-        if(*endptr != '\0' || limit_ > SIZE_MAX / 2)
+        if(*endptr != '\0')
         {
-            fprintf(stderr, "Argument 1 is unparseable or too large.\n");
+            fprintf(stderr, "Argument is unparseable.\n");
             return EXIT_FAILURE;
         }
         limit = limit_;
     }
-    // Strict upper bound till which primes will be calculated.
-    size_t limit_rounded = limit - limit % 60 + 60;
+
+    uint16_t *sieve = sieve_of_atkin(limit);
+    for(size_t i = 0; i <= limit; ++i)
+    {
+        int shift = shifts[i % 60];
+        if(shift == -1)
+        {
+            continue;
+        }
+        if((sieve[i / 60] >> shift & 1) == 1)
+        {
+            printf("%zu\n", i);
+        }
+    }
 
     return EXIT_SUCCESS;
 }
