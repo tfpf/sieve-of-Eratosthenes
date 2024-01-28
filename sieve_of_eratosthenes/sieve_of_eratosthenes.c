9cd56ba928a18fdce503dfde5f2482afbfffbf58
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-28 22:40:31 +0530

    Optimised sieve of Eratosthenes using wheel factorisation. (#2)

--- sieve_of_eratosthenes/sieve_of_eratosthenes.c
+++ sieve_of_eratosthenes/sieve_of_eratosthenes.c
@@ -1,44 +1,120 @@
-#include <errno.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <math.h>
+
+// Consecutive differences between coprime residues modulo 60: 1, 7, 11, 13,
+// 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53 and 59.
+static short unsigned OFFSETS[] = {6, 4, 2, 4, 2, 4, 6, 2, 6, 4, 2, 4, 2, 4, 6, 2};
+
+// Position of the bit indicating the primality of a coprime residue modulo 60
+// in a 16-element bitfield. For non-coprime residues, the value is 16.
+static short unsigned SHIFTS[] = {16, 0, 16, 16, 16, 16, 16, 1, 16, 16, 16, 2, 16, 3, 16, 16, 16, 4, 16, 5, 16, 16, 16, 6, 16, 16, 16, 16, 16,7, 16, 8, 16, 16, 16, 16, 16, 9, 16, 16, 16, 10, 16, 11, 16, 16, 16, 12, 16, 13, 16, 16, 16, 14, 16, 16, 16,16, 16, 15};
+
+struct SieveOfEratosthenes
+{
+    size_t limit;
+    size_t sieve_len;
+    // Each element of this array is a 16-element bitfield which indicates the
+    // primality of 16 coprime residues modulo 60. If a bit is cleared, the
+    // corresponding number is prime.
+    uint16_t *sieve;
+};
 
 /******************************************************************************
- * Find a bit in an array.
+ * Generate the sieve of Eratosthenes up to and including the given number.
  *
- * @param composite_array Array to look in.
- * @param position Position of the bit to look for.
+ * @param limit Non-strict upper bound up to which primes will be found.
  *
- * @return The bit.
+ * @return Sieve of Eratosthenes. This should be deleted after use.
  *****************************************************************************/
-bool composite_array_get(uint64_t *composite_array, int long long position)
+struct SieveOfEratosthenes *sieve_of_eratosthenes_new(size_t limit)
 {
-    int long long index = position / 64;
-    uint64_t mask = (uint64_t)1 << (position % 64);
-    return (composite_array[index] & mask) != 0;
+    struct SieveOfEratosthenes *soe = malloc(sizeof *soe);
+    if(soe == NULL)
+    {
+        fprintf(stderr, "Memory error. Fatally low memory.\n");
+        exit(EXIT_FAILURE);
+    }
+    soe->limit = limit;
+
+    // Strict upper bound divisible by 60.
+    size_t limit_rounded = limit - limit % 60 + 60;
+    if(limit_rounded <= limit)
+    {
+        fprintf(stderr, "Overflow detected. Argument is too large.\n");
+        exit(EXIT_FAILURE);
+    }
+    size_t limit_rounded_isqrt = sqrtl(limit_rounded);
+
+    // Start with a cleared array, indicating that all numbers are prime. Set
+    // the bit for 1, because it is not prime.
+    size_t sieve_len = limit / 60 + 1;
+    soe->sieve_len = sieve_len;
+    uint16_t *sieve = calloc(sieve_len, sizeof *sieve);
+    if(sieve == NULL)
+    {
+        fprintf(stderr, "Memory error. Argument is too large.\n");
+        exit(EXIT_FAILURE);
+    }
+    sieve[0] = 1;
+    soe->sieve = sieve;
+
+    // Set the bits for multiples of primes.
+    size_t num = 1;
+    size_t offsets_idx = 0;
+    for(size_t sieve_idx = 0; sieve_idx < sieve_len; ++sieve_idx)
+    {
+        for(int shift = 0; shift < 16; ++shift)
+        {
+            if((sieve[sieve_idx] >> shift & 1) == 0)
+            {
+                for(size_t multiple = num * num; multiple < limit_rounded; multiple += num)
+                {
+                    sieve[multiple / 60] |= 1 << SHIFTS[multiple % 60];
+                }
+            }
+            num += OFFSETS[offsets_idx];
+            offsets_idx = (offsets_idx + 1) % 16;
+            if(num > limit_rounded_isqrt)
+            {
+                return soe;
+            }
+        }
+    }
+    return soe;
 }
 
 /******************************************************************************
- * Change a bit in an array.
+ * Count the number of prime numbers which have been found.
  *
- * @param composite_array Array to look in.
- * @param position Position of the bit to change.
- * @param new_value Value to set the bit to.
+ * @param soe Sieve of Eratosthenes.
+ *
+ * @param Number of primes.
  *****************************************************************************/
-void composite_array_set(uint64_t *composite_array, int long long position, bool new_value)
+size_t sieve_of_eratosthenes_count(struct SieveOfEratosthenes *soe)
 {
-    int long long index = position / 64;
-    uint64_t mask = (uint64_t)1 << (position % 64);
-    if(new_value)
+    size_t num = 1;
+    size_t offsets_idx = 0;
+    size_t count = 0;
+    for(size_t sieve_idx = 0; sieve_idx < soe->sieve_len; ++sieve_idx)
     {
-        composite_array[index] |= mask;
+        for(int shift = 0; shift < 16; ++shift)
+        {
+            if((soe->sieve[sieve_idx] >> shift & 1) == 0)
+            {
+                ++count;
+            }
+            num += OFFSETS[offsets_idx];
+            offsets_idx = (offsets_idx + 1) % 16;
+            if(num > soe->limit)
+            {
+                return count;
+            }
+        }
     }
-    else
-    {
-        composite_array[index] &= ~mask;
-    }
+    return count;
 }
 
 /******************************************************************************
@@ -46,64 +122,20 @@ void composite_array_set(uint64_t *composite_array, int long long position, bool
  *****************************************************************************/
 int main(int const argc, char const *argv[])
 {
-    if(argc < 2)
+    size_t limit = 100;
+    if(argc > 1)
     {
-        fprintf(stderr, "Usage:\n");
-        fprintf(stderr, "  %s <number>\n", argv[0]);
-        fprintf(stderr, "where <number> is an integer greater than 1.\n");
-        return EXIT_FAILURE;
-    }
-
-    char *endptr;
-    errno = 0;
-    int long long limit = strtoll(argv[1], &endptr, 10);
-    if(*endptr != '\0' || errno == ERANGE || limit <= 1 || limit == LLONG_MAX)
-    {
-        fprintf(stderr, "Argument '%s' is invalid or out of range.\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-
-    // Allocate memory for an array. This array will be treated as a string of
-    // bits; each bit tells us whether the corresponding number is composite or
-    // prime.
-    int long long composite_array_size = (limit + 1) / 64;
-    if((limit + 1) % 64 != 0)
-    {
-        ++composite_array_size;
-    }
-    uint64_t *composite_array = calloc(composite_array_size, sizeof *composite_array);
-    if(composite_array == NULL)
-    {
-        fprintf(stderr, "Could not allocate %lld GiB of memory.\n", limit >> 33);
-        return EXIT_FAILURE;
-    }
-    fprintf(stderr, "Allocated approximately %lld KiB (%lld MiB) to store the sieve.\n", limit >> 13, limit >> 23);
-    composite_array_set(composite_array, 0, true);
-    composite_array_set(composite_array, 1, true);
-
-    // Initially, all bits are cleared. By the end, only the bits corresponding
-    // to prime numbers remain cleared.
-    for(int long long p = 2; p * p <= limit; ++p)
-    {
-        // If this number is prime, the corresponding bit in the array will be
-        // cleared. If so, mark all its multiples as composite.
-        if(!composite_array_get(composite_array, p))
+        char *endptr;
+        long long unsigned limit_ = strtoull(argv[1], &endptr, 10);
+        if(*endptr != '\0')
         {
-            // All multiples below the square of this number have already been
-            // processed. Just look at larger multiples.
-            for(int long long multiple = p * p; multiple <= limit; multiple += p)
-            {
-                composite_array_set(composite_array, multiple, true);
-            }
+            fprintf(stderr, "Argument is unparseable.\n");
+            return EXIT_FAILURE;
         }
+        limit = limit_;
     }
 
-    for(int long long p = 0; p <= limit; ++p)
-    {
-        if(!composite_array_get(composite_array, p))
-        {
-            printf("%lld\n", p);
-        }
-    }
-    return EXIT_SUCCESS;
+    struct SieveOfEratosthenes *soe = sieve_of_eratosthenes_new(limit);
+    size_t num_of_primes = sieve_of_eratosthenes_count(soe);
+    printf("%zu primes (excluding 2, 3 and 5) till %zu\n", num_of_primes, limit);
 }
