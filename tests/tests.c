010890044ec47409c68ee26367b39d4510f5de95
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-03-02 00:51:28 +0530

    Comment explaining test.

--- tests/tests.c
+++ tests/tests.c
@@ -8,10 +8,12 @@ main(void)
     struct SieveOfEratosthenes *erato = sieve_of_eratosthenes_new(1000000000);
     size_t erato_count = sieve_of_eratosthenes_count(erato);
     sieve_of_eratosthenes_delete(erato);
+    // The count excludes 2, 3 and 5.
     assert(erato_count == 50847531);
 
     struct SieveOfAtkin *atkin = sieve_of_atkin_new(1000000000);
     size_t atkin_count = sieve_of_atkin_count(atkin);
     sieve_of_atkin_delete(atkin);
+    // Ditto.
     assert(atkin_count == 50847531);
 }
